33885ef6c74fd296596392ddaeab0a152083b6d3
Mahima Saira Anoop <84089532+mahimasaira98@users.noreply.github.com>
2022-05-31 13:10:26 +0530

    Update transform-page.component.ts

--- transform-page.component.ts
+++ transform-page.component.ts
@@ -12,47 +12,7 @@ export class TransformPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  public displayArraylist = [
-    {
-      id: 1,
-      text: "first",
-      pos: 170
-    },
-    
-    {
-      id: 2,
-      text: "second",
-      pos: 20
-    },
-    {
-      id: 3,
-      text: "third",
-      pos: 3560
-    },
-    {
-      id: 4,
-      text: "fourth",
-      pos: 4
-    }
-  ]
-
-  applyFilter(id:number){
-    // this.displayArraylist = this.displayArraylist.filter((element) => {
-    //   return element.id !== id;
-    // })
-    // console.log(this.displayArraylist);
-    
-    let index = this.displayArraylist.findIndex((item) => item.id == 3)
-    console.log(index);
-    console.log(this.displayArraylist)
-
-    this.displayArraylist.sort((a,b) => {
-        return a.pos - b.pos
-        
-    })
-    console.log(this.displayArraylist)
+  
   }
   }
    
